b6cf9b8d7421d84fbbf45be81746eff858363ac6
Abhik Ray <abhikaboy@gmail.com>
2025-03-07 02:26:20 -0500

    init

---
+++ nix/shell.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.pandas
+      python-pkgs.requests
+      python-pkgs.numpy
+      python-pkgs.pytorch
+      python-pkgs.pymongo
+      python-pkgs.python-dotenv
+      python-pkgs.openai
+    ]))
+  ];
+  shellHook = ''
+    export PATH="$PATH:$(pwd)/backend"
+    export PYTHONPATH="$(pwd)/backend"    
+  '';
+}
